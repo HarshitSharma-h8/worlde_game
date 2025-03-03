3d4b553248fbf3dd9a894c3ed18a66055d75d905
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2025-03-03 19:16:30 +0530

    Adjust Header component padding for improved responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between px-[100px] py-[10px] border-b-1 border-gray-200 ">
+    <div className="flex items-center justify-between px-[20px] md:px-[100px] py-[10px] border-b-1 border-gray-200 ">
       <h1 className="text-xl dark:text-white">WORDLE</h1>
       <div className="flex items-center gap-5">
         <ThemeSwitcher />
